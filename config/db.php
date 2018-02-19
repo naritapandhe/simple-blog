ab0113105bff4cc5d23a03331b3582ee572259be
Narita Pandhe <narita.pandhe@gmail.com>
2018-02-19 12:51:02 -0800

    Moved DB creds

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
 <?php
+$keys = parse_ini_file('/Users/admin/Documents/cloud-elements/basic/secure/keys/config.ini', true);
 
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=blog',
-    'username' => 'root',
-    'password' => '',
+    'username' => $keys['db_username'],
+    'password' => $keys['db_password'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
